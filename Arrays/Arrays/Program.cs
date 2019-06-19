3b8b92a780d7c26cc2505ea1192aff61d50ac7e8
Federico <fedegv.4991@gmail.com>
2019-06-19 00:13:27 -0300

    Ejemplo Arrays

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -10,6 +10,67 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
+            // array implicito
+            /*
+            // var datos = new[] {"Juan", "Perez", "Argentina"};
+            var valores = new[] { 15, 28, 35, 75.5, 30.30 };
+            */
+            // array de objetos
+            Empleados[] arrayEmpleados = new Empleados[10];
+            
+            // Creacion objeto y asignacion misma linea
+            arrayEmpleados[0] = new Empleados("Sara", 37);
+
+            // Creacion objeto y asignacion a array
+            Empleados ana = new Empleados("Ana", 24);
+            arrayEmpleados[1] = ana;
+
+
+            arrayEmpleados[2] = new Empleados("Monty", 13);
+            arrayEmpleados[3] = new Empleados("Robert", 22);
+
+            // array de tipos o clases anonimas
+            /*
+            var personas = new[]
+            {
+                new {Nombre="Juan", Edad= 19},
+                new { Nombre = "Martin", Edad = 22 },
+                new { Nombre = "Diana", Edad = 35 }
+            };
+            */
+
+            for (int i = 0; i < arrayEmpleados.Length; i++)
+            {
+
+                //Console.WriteLine("Nombre: {0} Edad: {1}",arrayEmpleados[i].getNombre, arrayEmpleados[i].getEdad);
+                try
+                {
+                    Console.WriteLine(arrayEmpleados[i].getInfo());
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine("Fuera de rango");
+                }
+                
+            }
+        }
+    }
+    class Empleados
+    {
+        // CAMPO DE CLASE
+        private string nombre;
+        private int edad;
+
+        // CONSTRUCTOR
+        public Empleados(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public string getInfo()
+        {
+            return "Nombre: "+ nombre + " Edad: "+edad;
         }
     }
 }
